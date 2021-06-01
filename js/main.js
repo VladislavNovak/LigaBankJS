64a3ace6ad389105274b73df1a7a5c25e1c17d39
Valdix <novaldix@gmail.com>
2021-06-01 10:42:08 +0300

    Подготавливает проект под React

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 async function getCurrencies (dateQuery = 'https://www.cbr-xml-daily.ru/daily_json.js') {
   const response = await fetch(dateQuery);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   const result = await data;
   
   rates.date = result.Date;
@@ -13,8 +13,10 @@ async function getCurrencies (dateQuery = 'https://www.cbr-xml-daily.ru/daily_js
 }
 
 let previousURL;
+var pattern = /^[-0-9.,]+$/;
 const currentDate = new Date();
 let selectedDate = new Date();
+let isErrorValidity = false;
 
 const orderType = {
   LEFT: `LEFT`,
@@ -26,17 +28,18 @@ let currentAction = {};
 const history = [];
 
 const bodyElement = document.body;
-// Элементы для отображения курса валют:
 const cashInput = bodyElement.querySelector(`.sums__cash`);
 const cashType = bodyElement.querySelector(`.sums__cash-type`);
 const exchangedInput = bodyElement.querySelector(`.sums__exchanged`);
 const exchangedType = bodyElement.querySelector(`.sums__exchanged-type`);
 
+const sumsSubmit = bodyElement.querySelector(`.sums`);
+
 const datePicker = bodyElement.querySelector(`.flatpickr__setdate`);
 datePicker.valueAsDate = currentDate;
 
-datePicker.addEventListener("change", (evt) => {
-  selectedDate = evt.target.value;
+datePicker.addEventListener("change", ({target}) => {
+  selectedDate = target.value;
   console.log(`currentDate: `, currentDate);
   console.log(`selectedDate: `, selectedDate);
   // if(currentDate > selectedDate) {
@@ -47,14 +50,22 @@ datePicker.addEventListener("change", (evt) => {
 
 getCurrencies();
 
-// Получает: 
-// инпут, на котором произошло событие,
-// инпут, в котором необходимо менять сумму,
-// два селекта с типом национальной валюты,
-// тип формирования объекта currentAction,
+const setAppearanceOfHashtagField = () => {
+  if (isErrorValidity) {
+    cashInput.classList.add(`sums__input--error`);
+    exchangedInput.classList.add(`sums__input--error`);
+  } else {
+    cashInput.classList.remove(`sums__input--error`);
+    exchangedInput.classList.remove(`sums__input--error`);
+  }
+};
+
 const convert = (selfInput, otherInput, selfValuteType, otherValuteType, type) => {
+  isErrorValidity = false;
+  setAppearanceOfHashtagField();
   const selfType = selfValuteType.options[selfValuteType.selectedIndex].value;
   const otherType = otherValuteType.options[otherValuteType.selectedIndex].value;
+
   const cash = Number(selfInput.value);
   const rur = cash / (rates[otherType] ? rates[otherType].Value : 1);
   const converted = Math.floor(rur * (rates[selfType] ? rates[selfType].Value : 1) * 100) / 100;
@@ -62,11 +73,18 @@ const convert = (selfInput, otherInput, selfValuteType, otherValuteType, type) =
   otherInput.value = converted;
 
   currentAction = (type === orderType.LEFT) 
-    ? {...currentAction, cash1: cash, selfType1: selfType, converted1: converted, otherType1: otherType} 
-    : {...currentAction, cash1: converted, selfType1: otherType, converted1: cash, otherType1: selfType};
+    ? {...currentAction, currentDate, firstCash: cash, firstCashType: selfType, secondCash: converted, secondCashType: otherType} 
+    : {...currentAction, currentDate, firstCash: converted, firstCashType: otherType, secondCash: cash, secondCashType: selfType};
+}
+
+const onInputBlur = ({target}) => {
+  if (pattern.exec(target.value)) {
+    target.value = parseFloat(target.value.replace(/\,/g, '.'));
+  }
 }
 
 const onInputSum = (otherInput, selfValuteType, otherValuteType,type) => (evt) => {
+
   convert(evt.target, otherInput, selfValuteType, otherValuteType, type)
 }
 
@@ -79,3 +97,33 @@ exchangedInput.addEventListener("input", onInputSum(cashInput, exchangedType, ca
 
 cashType.addEventListener("change", onChangeType(cashInput, exchangedInput, exchangedType, orderType.RIGHT));
 exchangedType.addEventListener("change", onChangeType(exchangedInput, cashInput, cashType, orderType.LEFT));
+
+cashInput.addEventListener("blur", onInputBlur);
+exchangedInput.addEventListener("blur", onInputBlur);
+
+sumsSubmit.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  if (!Number(cashInput.value) && !Number(exchangedInput.value)) {
+    isErrorValidity = true;
+    setAppearanceOfHashtagField();
+    return;
+  }
+
+  history.push(currentAction);
+  console.log(history);
+  if (history.length > 10) {
+    history.shift();
+  }
+});
+
+// currentAction: позволяет отслеживать текущие данные. Содержит:
+// - выбранную дату, данные инпутов и селектов
+// getCurrencies: запрос на сервер для получения текущего курса валют:
+// - по умолчанию запрос идёт по стандартному адресу. Либо, на прошедшую дату
+// setAppearanceOfHashtagField: проверяет isErrorValidity, если true, устанавливает красную рамку
+// convert получает:
+// - инпут, на котором произошло событие,
+// - инпут, в котором необходимо менять сумму,
+// - два селекта с типом национальной валюты,
+// - тип формирования объекта currentAction,
+// - формирует currentAction
